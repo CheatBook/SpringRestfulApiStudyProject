ffd52303d3aea92939ac23915ae85e8071e41eab
kou <km1994321@yahoo.co.jp>
2021-03-20 19:46:09 +0900

    CRAD操作学習用プロジェクト作成

---
+++ spring-rest1-2/src/main/java/dev/itboot/rest/SpringRest12Application.java
@@ -0,0 +1,13 @@
+package dev.itboot.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRest12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRest12Application.class, args);
+	}
+
+}
