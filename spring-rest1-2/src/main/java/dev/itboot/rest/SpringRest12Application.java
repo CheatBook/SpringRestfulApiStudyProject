46b5dba767bcf9142c8c3812b03b7eb5b9873a54
kou <km1994321@yahoo.co.jp>
2021-03-20 20:55:58 +0900

    メインファイルのタブをスペースに変更

--- spring-rest1-2/src/main/java/dev/itboot/rest/SpringRest12Application.java
+++ spring-rest1-2/src/main/java/dev/itboot/rest/SpringRest12Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringRest12Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRest12Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRest12Application.class, args);
+    }
 
 }
