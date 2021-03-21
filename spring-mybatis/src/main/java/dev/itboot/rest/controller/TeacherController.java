8eb976ee2d121c458651983ec59f137e94a8dae3
kou <km1994321@yahoo.co.jp>
2021-03-21 23:38:23 +0900

    コントローラにCRAD追加

--- spring-mybatis/src/main/java/dev/itboot/rest/controller/TeacherController.java
+++ spring-mybatis/src/main/java/dev/itboot/rest/controller/TeacherController.java
@@ -2,22 +2,54 @@ package dev.itboot.rest.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import dev.itboot.rest.mapper.TeacherMapper;
+import dev.itboot.rest.model.Teacher;
+import dev.itboot.rest.service.TeacherService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
 public class TeacherController {
-    
-    private final TeacherMapper mapper;
-    
+
+    private final TeacherService service;
+
     @GetMapping("/")
     public String getAllTeachers(Model model) {
-        model.addAttribute("page", mapper.selectAll());
-        
+        model.addAttribute("page", service.findAll());
         return "list";
-
     }
+
+    @GetMapping("/add")
+    public String addTeacherString(@ModelAttribute Teacher teacher) {
+        return "form";
+    }
+
+    @PostMapping("/process")
+    public String process(@Validated @ModelAttribute Teacher teacher, BindingResult result) {
+        if (result.hasErrors()) {
+            return "form";
+        }
+        service.save(teacher);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editTeacher(@PathVariable Long id, Model model) {
+        model.addAttribute("teacher", service.findById(id));
+        return "from";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteTeacher(@PathVariable Long id) {
+        service.deleteById(id);
+        return "redirect:/";
+    }
+
 }
