12e3a716f56995cd1495907881f8cd857a7f515c
kou <km1994321@yahoo.co.jp>
2021-03-22 06:49:05 +0900

    教材の答えをすべて反映

---
+++ answer/spring-boot-adv-2.4/spring-mybatis-pagination/src/main/java/dev/itboot/mb/config/DataLoader.java
@@ -0,0 +1,25 @@
+package dev.itboot.mb.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import dev.itboot.mb.model.Teacher;
+import dev.itboot.mb.service.TeacherService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final TeacherService service;
+
+    @Override
+    public void run(String... args) throws Exception {
+        for (int i = 0; i < 5; i++) {
+            Teacher teacher = new Teacher();
+            teacher.setUserName("テスト");
+            teacher.setEmail("test@example.com");
+            service.save(teacher);
+        }
+    }
+}
