2e9663e35e1ca5eb48011ff0504c05c41656cecf
kou <km1994321@yahoo.co.jp>
2021-03-21 10:04:42 +0900

    Controller作成

---
+++ spring-rest3/src/main/java/dev/itboot/rest/controller/v1/TaskController.java
@@ -0,0 +1,68 @@
+package dev.itboot.rest.controller.v1;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.itboot.rest.repository.TaskRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+
+import dev.itboot.rest.model.Task;
+
+@RequestMapping("/api/v1/tasks")
+@RequiredArgsConstructor
+@RestController
+public class TaskController {
+
+    private final TaskRepository repository;
+
+    @CrossOrigin
+    @Operation(summary = "タスクを全件取得します")
+    @GetMapping("/")
+    List<Task> findAll() {
+        return repository.findAll();
+
+    }
+
+    @Operation(summary = "タスクを登録します")
+    @PostMapping("/")
+    Task save(@RequestBody Task task) {
+        return repository.save(task);
+    }
+
+    @Operation(summary = "タスクを1件取得します")
+    @GetMapping("/{id}")
+    Task findById(@PathVariable Long id) {
+        return repository.findById(id).get();
+    }
+
+    @Operation(summary = "タスクを更新します")
+    @PutMapping("/{id}")
+    Task save(@RequestBody Task newTask, @PathVariable Long id) {
+
+        return repository.findById(id).map(task -> {
+            task.setName(newTask.getName());
+            task.setCompleted(newTask.getCompleted());
+            return repository.save(task);
+        }).orElseGet(() -> {
+            newTask.setId(id);
+            return repository.save(newTask);
+        });
+    }
+
+    @Operation(summary = "タスクを削除します")
+    @DeleteMapping("/{id}")
+    void deleteById(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+}
