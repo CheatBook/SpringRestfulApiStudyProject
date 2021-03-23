e35335b5051155b3ca76bda1b88c662fd2403dc9
kou <cheatbook_tarou@yahoo.co.jp>
2021-03-24 06:11:32 +0900

    データを100件挿入するCommandLineRunnerを追加

---
+++ spring-mybatis/src/main/java/dev/itboot/rest/config/DataLoader.java
@@ -0,0 +1,25 @@
+package dev.itboot.rest.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import dev.itboot.rest.model.Teacher;
+import dev.itboot.rest.service.TeacherService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class DataLoader implements CommandLineRunner {
+    private final TeacherService service;
+
+    @Override
+    public void run(String... args) throws Exception {
+        for (int i = 0; i < 100; i++) {
+            Teacher teacher = new Teacher();
+            teacher.setUserName("テスト");
+            teacher.setEmail("test@example.com");
+            service.save(teacher);
+        }
+    }
+
+}
